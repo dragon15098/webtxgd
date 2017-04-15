5217d78f4601366efec2a776ec417c131df81267
DESKTOP-F6DG3P0\HP <dragon15098@gmail.com>
2017-04-15 16:54:02 +0700

    no message

--- models/user.py
+++ models/user.py
@@ -4,5 +4,7 @@ class User(Document):
     username = StringField()
     password = StringField()
     token = StringField()
-
+    gender = StringField()
     description = StringField()
+    number = IntField()
+    image = StringField()
